22d17f2893b7102893f6f605dd6e96d8415c1c25
Aditya <121721549+adityaSalame@users.noreply.github.com>
2023-06-30 21:07:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+async function callme(){
+	const message= document.getElementById("text").value;
+	const delay= parseInt(document.getElementById("delay").value);
+	await delayfunc(delay,message);
+	 
+	
+}
+function delayfunc(delay,message) {
+  return new Promise(resolve=>{
+	  
+	  setTimeout(resolve,delay)
+	  document.getElementById("output").innerText=message;
+  })
+}
+ document.getElementById("btn").addEventListener("click",callme);
